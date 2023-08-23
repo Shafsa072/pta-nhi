7cdec4e9d1267323e02bb57da46191607c59405e
Shafsa072 <shafsa072@gmail.com>
2023-08-23 08:15:49 -0700

    done

---
+++ class.py
@@ -0,0 +1,96 @@
+# questions =[{
+#     "S.NO" = "1", 
+#     "question" = "what is your name?",
+#     "options" = ["ali" , "ahmed" , "adnan"],
+#     "answer" = "ali"
+# },
+# {
+#     "S.NO" = "2", 
+#     "question" = "what is your age?",
+#     "options" = ["18" , "22" , "24"],
+#     "answer" = "22"
+# },
+# {
+#     "S.NO" = "3", 
+#     "question" = "what is your city  name?",
+#     "options" = ["karachi" , "multan ", "lahore"],
+#     "answer" = "karachi"
+# }]
+
+# score = 0
+# for quiz in questions:
+    
+#     data = (quiz["S.NO"] + "," +" " + quiz["question"] + "," + " " +"/n" + quiz["options"][0] + "/n"
+#     + quiz["options"][1] + "/n" +  quiz["options"][2] + "/n"+ quiz["answer"])
+
+#     answer = input(data)
+#     if answer == quiz["answer"]:
+#         score += 1
+# print("your score is",str(score))
+
+
+
+# function
+# def add():
+#     a = 10
+#     b = 15
+#     print(a +b)
+
+# add()
+
+# def greet():
+#     print("hello! how are u?")
+
+# greet()
+
+# def addition(a,b):
+#     print(a + b)
+# addition(15 , 10)
+
+# def newadd(a=5 ,b=8):
+#    print(a + b)
+# newadd()
+
+# def fullName(firstName , lastName):
+#     print(firstName + " " + lastName)
+
+# fullName("hafsa","fatima")
+
+
+# function (return keyword)
+
+
+
+
+# Write a Python program which accepts the radius of a circle from the user and compute
+# the area. 
+
+
+# radius = float(input(" enter radius"))
+# area = 3.14*radius**2
+# print(area)
+
+
+# r = float(input("enter radius"))
+# a = 3.14*r**2
+# print(a)
+
+
+
+
+
+# 3. Write a Python program to display the current date and time. 
+
+# import datetime 
+
+# y = datetime.datetime.now()
+# print(y)
+
+# Write a Python program which accepts the user's first and last name and print them in
+# reverse order with a space between them
+# firstName = input("enter your fristName")
+# lastName = input("enter your lastName")
+# print(lastName + firstName)
+
+
+# Write a Python program to sum all the numeric items in a list?
